de610af9e5bf8c0280d090d65970d3af3e645781
halim-kim <goflsms2000@gmail.com>
2024-05-21 20:25:16 +0900

    #12

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,18 +1,32 @@
-import dummy from '../db/data.json';
+// import dummy from '../db/data.json';
 import { useParams } from 'react-router-dom';
 import Word from './Word';
+import { useEffect, useState } from 'react';
 
 export default function Day() {
   const { day } = useParams(); 
   const dayInt = parseInt(day, 10); 
-  const wordList = dummy.words.filter(word => word.day === dayInt);
+//   const wordList = dummy.words.filter(word => word.day === dayInt);
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/words?day=${day}`)
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        setWords(data);
+    });
+    
+}, [day]); 
+
 
   return (
     <>
       <h2>Day {dayInt}</h2>
       <table>
         <tbody>
-          {wordList.map(word => (
+          {words.map(word => (
             <Word word={word} key={word.id}/>
           ))}
         </tbody>
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,16 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; 
-import dummy from '../db/data.json';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function DayList() {
-  console.log(dummy);
+    const [days, setDays] = useState([]);
+    // const [count, setCount] = useState(0);
+
+    // function onClick() {
+    //     setCount(count + 1);
+    // }
+
+    // function onClick2() {
+    //     setDays([
+    //         ...days,
+    //         { id: Math.random(), day: 1 },
+    //     ])
+    // }
+
+    useEffect(() => {
+        fetch('http://localhost:3001/days')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setDays(data);
+        });
+        
+    }, []); // count 값이 변경될 때만 실행, 빈배열이면 처음 한번만 실행
+    
+
   return (
-    <ul className="list_day">
-      {dummy.days.map(day => (
-        <li key={day.id}>
-          <Link to={`/day/${day.day}`}>Day {day.day}</Link> 
-        </li>
-      ))}
-    </ul>
+    // <>
+        <ul className="list_day">
+        {days.map(day => (
+            <li key={day.id}>
+            <Link to={`/day/${day.day}`}>Day {day.day}</Link> 
+            </li>
+        ))}
+        </ul>
+    //     <button onClick={onClick}>{count}</button>
+    //     <button onClick={onClick2}>Day Change</button>
+    // </>
   );
 }
