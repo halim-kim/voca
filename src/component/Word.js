f0fbb02b7f10792055c4bf316b7689a9b1a50458
halim-kim <goflsms2000@gmail.com>
2024-05-22 00:19:31 +0900

    add del function and learned how to restart json

--- src/component/Word.js
+++ src/component/Word.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
-export default function Word({ word }) {
+export default function Word({ word: w }) {
+    const [word, setWord] = useState(w);
     const [isShow, setIsShow] = useState(false);
     const [isDone, setIsDone] = useState(word.isDone);
 
@@ -9,21 +10,37 @@ export default function Word({ word }) {
     }
 
     function handleDone() {
-      fetch(`http://localhost:3001/words/${word.id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          ...word,
-          isDone: !isDone,
-        }),
-      }).then(res => {
-        if (res.ok) {
-          setIsDone(!isDone);
-        }
-      });
+        // setIsDone(!isDone);
+        fetch(`http://localhost:3001/words/${word.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ ...word, isDone: !isDone }),
+        })
+        .then(res => {
+            if (res.ok) {
+                setIsDone(!isDone);
+            }
+        });
     }
+
+
+    function del() {
+        if (window.confirm('削除しますか？')) {
+        fetch(`http://localhost:3001/words/${word.id}`, {
+            method: 'DELETE',
+        }).then(res => {
+            if (res.ok) {
+                setWord({ id: 0 });
+            }
+        });
+      }
+    }
+
+  if (word.id === 0) {
+    return null;
+  }
 
   return (
     <tr className={isDone ? "off" : ""}>
@@ -35,7 +52,7 @@ export default function Word({ word }) {
       <td>{isShow && word.jap}</td>
       <td>
         <button onClick={handleShow}>意味を{isShow ? "隠す" : "確認"}</button>
-        <button className="btn_del">削除</button>
+        <button onClick={del} className="btn_del">削除</button>
       </td>
     </tr>
   );
