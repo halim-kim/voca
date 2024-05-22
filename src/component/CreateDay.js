dc3f1fe4d3068931043b6ed0bad53866cdd87274
halim-kim <goflsms2000@gmail.com>
2024-05-22 21:36:54 +0900

    create daylist

--- src/component/CreateDay.js
+++ src/component/CreateDay.js
@@ -1,11 +1,34 @@
+import useFetch from "../hooks/useFetch";
+import { useNavigate } from 'react-router-dom';
+
 export default function CreateDay() {
+    const days = useFetch('http://localhost:3001/days');
+    const navigate = useNavigate();
+
+    function addDay(){
+            fetch('http://localhost:3001/days', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    day:days.length + 1
+                }),
+            })
+            .then(res => {
+                if (res.ok) {
+                    alert("登録が完了しました");
+                    navigate(`/`);
+                }
+            });
+        }
     return (
         <div>
             <h3>Day 追加</h3>
             <form>
                 <input type="text" />
-                <button>Submit</button>
+                <button onClick={addDay}>Submit</button>
             </form>
         </div>
-    )
+    );
 }
